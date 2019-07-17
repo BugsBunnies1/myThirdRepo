c8086a9cbe0ed154cfadf9b619d844d51fe091c9
BugsBunnies11 <brankojaksic91@gmail.com>
2019-07-17 07:34:11 +0200

    Created BOMForApartmentWindow.cs

---
+++ myThirdProj/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+* ******************************************
+* EXAMPLE:                                 *
+*          Simple app for calculation of   *
+*          material spent for installing   *
+*          apartment or house windows      *
+********************************************
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace myThirdProj
+{
+    class BOWForApartmentWindow
+    {
+        static void Main(string[] args)
+        {
+            // variable declaration
+            double height, width, woodLength, glassArea;
+            string heightString, widthString;
+
+            // variable initialization
+            Console.WriteLine("Please enter HEIGHT of the window (meters): ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            Console.WriteLine("Please enter WIDTH of the window (meters): ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            // calculation
+            woodLength = 2 * (height + width) * 3.25;
+            glassArea = 2 * (height + width);
+
+            // result
+            Console.WriteLine("The length of the wood is " + woodLength + " feet.");
+            Console.WriteLine("The area of the glass is " + glassArea + " square meters.");
+        }
+    }
+}
